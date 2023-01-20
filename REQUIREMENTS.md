5f449923d2c631e08de48cee0cabbe7443f43a52
amira <122740806+amira-codecamp@users.noreply.github.com>
2023-01-21 00:15:25 +0100

    pdf

---
+++ REQUIREMENTS.md
@@ -0,0 +1,8 @@
+# Packages
+pip install django-wkhtmltopdf
+
+# Change path in settings.py
+WKHTMLTOPDF_CMD = '/path/to/my/wkhtmltopdf'
+
+# 
+https://pypi.org/project/django-wkhtmltopdf/
--- reco/polls/views.py
+++ reco/polls/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from wkhtmltopdf import render_pdf_from_template
 
 def index(request):
     context = {}
@@ -8,7 +8,25 @@ def index(request):
 
 def pdf(request):
     context = {}
-    return render(request, 'reco/templates/pdf.html', context)
+    data = request.POST
+    context['user'] = data.get('user')
+    context['company'] = data.get('company')
+    context['email'] = data.get('email')
+    context['candidate'] = data.get('candidate')
+    context['position'] = data.get('position')
+    context['motivation'] = data.get('position')
+    context['solving'] = data.get('solving')
+    context['communication'] = data.get('communication')
+    context['teamwork'] = data.get('teamwork')
+    context['adaptability'] = data.get('adaptability')
+    context['creativity'] = data.get('creativity')
+    context['text'] = data.get('text')
+    template_name = 'reco/templates/pdf.html'
+    cmd_options={'orientation':'Portrait', 'page-size':'A4' }
+    pdf_ = render_pdf_from_template(template_name, header_template=None, footer_template=None, context=context, cmd_options=cmd_options, request=request)
+    response = HttpResponse(pdf_, content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="reference-letter.pdf"'
+    return response
 
 def word(request):
     context = {}
