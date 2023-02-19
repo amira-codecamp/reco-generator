94a39b61052bff2a38df43228d7989999da2b0ea
amira <122740806+amira-codecamp@users.noreply.github.com>
2023-02-19 22:01:48 +0100

    -

--- reco/polls/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('index/', views.index, name='main-view'),
-    path('pdf/', views.pdf, name='pdf'),
-    path('word/', views.word, name='word'),
-]
